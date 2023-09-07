ab09ffb3bceedaa38c8a92881a2f858f67c6fe00
M-Gridge <mainaianndungu@gmail.com>
2023-09-07 12:11:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A Programe printing a line with put fun
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
